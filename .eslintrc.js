0cdd1193c7eff8c648396a42ab7470aa6dc7586e
Adrian Gruber <adrian_gruber@epam.com>
2020-02-19 12:28:54 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': 'off',
+    'import/extensions': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ components/NavigationBar.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const NavigationBar = styled.nav`
+  color: palevioletred;
+  background: papayawhip;
+`;
+
+export default NavigationBar;
